aa630e9f0c74395a32e61338647a428b3fe9926e
Dmitry Litun <dlitun@ya.ru>
2026-03-08 17:39:11 +0300

    feat(RCA-06): add color scheme

---
+++ app/src/main/java/com/example/recipecomposeapp/ui/theme/Dimens.kt
@@ -0,0 +1,31 @@
+package com.example.recipecomposeapp.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+
+    // Spacing
+    val Space4 = 4.dp
+    val Space8 = 8.dp
+    val Space16 = 16.dp
+    val Space24 = 24.dp
+    val Space32 = 32.dp
+
+    // Corners
+    val CornerSmall = 8.dp
+    val CornerMedium = 12.dp
+    val CornerLarge = 16.dp
+
+    // Elevation
+    val ElevationSmall = 2.dp
+    val ElevationMedium = 4.dp
+
+    // Icons
+    val IconMedium = 24.dp
+
+    // Buttons
+    val ButtonHeight = 48.dp
+
+    // Slider
+    val SliderTrackHeight = 4.dp
+}
