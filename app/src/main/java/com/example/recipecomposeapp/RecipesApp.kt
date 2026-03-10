6710deeef02444f6ae5460ff4df1d5f1c2ce8162
Dmitry Litun <dlitun@ya.ru>
2026-03-11 00:04:30 +0300

    refactor(theme): rename RecipeComposeAppTheme to RecipesAppTheme

--- app/src/main/java/com/example/recipecomposeapp/RecipesApp.kt
+++ app/src/main/java/com/example/recipecomposeapp/RecipesApp.kt
@@ -6,11 +6,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.recipecomposeapp.ui.theme.RecipeComposeAppTheme
+import com.example.recipecomposeapp.ui.theme.RecipesAppTheme
 
 @Composable
 fun RecipesApp() {
-    RecipeComposeAppTheme {
+    RecipesAppTheme {
         Scaffold { paddingValues ->
             Text(
                 text = "Recipes App",
